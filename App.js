a1f2258864a086a09e6fe01a34089cb41c7546f8
Evaldas88 <e.skackauskas@gmail.com>
2023-04-11 12:05:03 +0300

    added animation for button, movie page

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import React from 'react';
  import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
+import Movie from './screens/Movie';
 
 
 
@@ -17,6 +18,7 @@ export default function App() {
       <NavigationContainer>
         <Stack.Navigator screenOptions={{headerTitleAlign: 'center'}}>
           <Stack.Screen   name="Home" component={HomeScreen} />
+          <Stack.Screen   name="Movie" component={Movie} />
         </Stack.Navigator>
       </NavigationContainer>
     </TailwindProvider>
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,18 +1,14 @@
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/native'
-import { SafeAreaView, View, Text } from 'react-native';
-
+import { SafeAreaView, View, Text, Image, TouchableOpacity } from 'react-native';
+import * as Animatable from 'react-native-animatable';
 
 
 const HomeScreen = () => {
 
     const navigation = useNavigation();
 
-    useLayoutEffect(() => {
-        navigation.setOptions({
-            headerShown: false,
-        })
-    }, [])
+ 
 
 
 
@@ -27,13 +23,37 @@ const HomeScreen = () => {
             </View>
             {/* Second Section */}
 
-        <View>
-            <Text>CIa bus tekstas</Text>
-        </View>
-
-
-
-        </SafeAreaView>
+            <View className="px-6 mt-8 space-y-3">
+                <Text className=" text-[#3C6072] text-[42px]" >CIa bus tekstas</Text>
+                <Text className=" text-[#3C6072] text-[38px] font-bold" >Cia tekstas</Text>
+                <Text className="text-[#3C6072]">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</Text>
+            </View>
+            {/* Circle Section */}
+            <View className="w-[300px] h-[300px] bg-[#4DABB7] rounded-full absolute bottom-36 -right-36"></View>
+            <View className="w-[300px] h-[300px] bg-[#fab01e] rounded-full absolute -bottom-28 -left-28"></View>
+            {/* Image container */}
+            <View className="flex-1 relative items-center justify-center">
+                <Animatable.Image
+                    animation="pulse"
+                    easing="ease-in-out"
+                    source={{
+                        uri: "https://cdn.pixabay.com/photo/2015/06/30/08/07/lens-826308_1280.jpg",
+                    }}
+                    className="w-[350px] h-[350px] object-cover  rounded-full" />
+                <TouchableOpacity 
+                onPress={() => navigation.navigate("Movie")}
+                className="absolute bottom-20 w-24 h-24 border-l-2 border-r-2 border-t-4 border-[#1b4714] rounded-full items-center justify-center">
+                        <Animatable.View
+                            animation={"pulse"}
+                            easing="ease-in-out"
+                            iterationCount={"infinite"}
+                            className="w-20 h-20 justify-center items-center rounded-full  bg-[#1b4714]">
+                            <Text className="text-[#da9b54] text-[20px] font-semibold">Browse</Text>
+                        </Animatable.View>
+                </TouchableOpacity>
+            </View>
+        </SafeAreaView >
     )
 }
 
